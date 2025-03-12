3e530eed874c34bddb2445d51e45b355260d80c3
Caiosfs <caioviniciussfs00@gmail.com>
2025-03-12 21:23:36 +0000

    Teste 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -40,7 +40,7 @@ int main() {
     printf("Pontos Turisticos: ");
     scanf("%d", &turis1);
 
-    printf("Cadastro de Cartas.\n");
+    printf("\nCadastro de Cartas.\n");
 
     printf("Código da Cidade: ");
     scanf("%s", cdd_cod2);
@@ -61,8 +61,8 @@ int main() {
     scanf("%d", &turis2);
 
 
-    printf("Carta %c.\nCódigo da Cidade: %s\nCidade: %s\nPopulação: %d\nPIB: %.2f\nÁrea: %.2f km²\nPontos Turisticos: %d\n", cdd_cod1, cdd1, pop1, pib1, area1, turis1);
-    printf("\nCarta %c.\nCódigo da Cidade: %s\nCidade: %s\nPopulação: %d\nPIB: %.2f\nÁrea: %.2f km²\nPontos Turisticos: %d\n", cdd_cod2, cdd2, pop2, pib2, area2, turis2);
+    printf("\nCarta %c.\nCódigo da Cidade: %s\nCidade: %s\nPopulação: %d\nPIB: %.2f\nÁrea: %.2f km²\nPontos Turisticos: %d\n", codcard1, cdd_cod1, cdd1, pop1, pib1, area1, turis1);
+    printf("\nCarta %c.\nCódigo da Cidade: %s\nCidade: %s\nPopulação: %d\nPIB: %.2f\nÁrea: %.2f km²\nPontos Turisticos: %d\n", codcard2, cdd_cod2, cdd2, pop2, pib2, area2, turis2);
 
     return 0;
 }
